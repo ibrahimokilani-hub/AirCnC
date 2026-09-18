2c27bae1bbd4a73f13fde3d796c4da2c8eb7a624
ibrahimokilani-hub <ibrahimo.kilani@gmail.com>
2026-09-18 10:23:20 +0300

    feat(swagger): integrate Swagger for API documentation

---
+++ src/HotelBooking.Api/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace HotelBooking.Api.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+    public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+        });
+
+        return app;
+    }
+}
--- src/HotelBooking.Api/Program.cs
+++ src/HotelBooking.Api/Program.cs
@@ -1,9 +1,11 @@
+using HotelBooking.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddSwaggerDocumentation();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerDocumentation();
 }
 
 app.UseHttpsRedirection();
